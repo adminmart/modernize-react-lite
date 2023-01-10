04e6aab1e710e727ca9d89ebce31cd0567688a57
shraddha <wrappixelteam.2016@gmail.com>
2023-01-10 12:28:31 +0530

    ''

--- src/layouts/full/header/Header.js
+++ src/layouts/full/header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, AppBar, Toolbar, styled, Stack, IconButton, Badge } from '@mui/material';
+import { Box, AppBar, Toolbar, styled, Stack, IconButton, Badge, Button } from '@mui/material';
 import PropTypes from 'prop-types';
 
 // components
@@ -43,25 +43,29 @@ const Header = (props) => {
           <IconMenu width="20" height="20" />
         </IconButton>
 
+
+        <IconButton
+          size="large"
+          aria-label="show 11 new notifications"
+          color="inherit"
+          aria-controls="msgs-menu"
+          aria-haspopup="true"
+          sx={{
+            ...(typeof anchorEl2 === 'object' && {
+              color: 'primary.main',
+            }),
+          }}
+        >
+          <Badge variant="dot" color="primary">
+            <IconBellRinging size="21" stroke="1.5" />
+          </Badge>
+
+        </IconButton>
         <Box flexGrow={1} />
         <Stack spacing={1} direction="row" alignItems="center">
-          <IconButton
-            size="large"
-            aria-label="show 11 new notifications"
-            color="inherit"
-            aria-controls="msgs-menu"
-            aria-haspopup="true"
-            sx={{
-              ...(typeof anchorEl2 === 'object' && {
-                color: 'primary.main',
-              }),
-            }}
-          >
-            <Badge variant="dot" color="primary">
-              <IconBellRinging size="21" stroke="1.5" />
-            </Badge>
-
-          </IconButton>
+          <Button variant="contained" color="primary"  target="_blank" href="https://adminmart.com/product/modernize-react-mui-dashboard-template/">
+            Upgrade to Pro
+          </Button>
           <Profile />
         </Stack>
       </ToolbarStyled>
