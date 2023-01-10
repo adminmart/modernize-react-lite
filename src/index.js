fcd980f809dfe9da22ca3c9eca7764f0f800e654
niravjoshi <niravjoshi87@gmail.com>
2023-01-10 09:38:14 +0530

    ''

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Suspense>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Suspense>
+);
