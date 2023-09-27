b7c1230220e91e38cca5fd1112d54fa4849c6321
wrappixelTeam <wrappixelteam.2016@gmail.com>
2023-09-27 17:24:52 +0530

    ''

--- package/src/layouts/full/shared/logo/Logo.js
+++ package/src/layouts/full/shared/logo/Logo.js
@@ -1,20 +1,27 @@
-import { Link } from 'react-router-dom';
-import { ReactComponent as LogoDark } from 'src/assets/images/logos/dark-logo.svg';
-import { styled } from '@mui/material';
+import { Link } from "react-router-dom";
+import { ReactComponent as LogoDark1 } from "src/assets/images/logos/dark1-logo.svg";
+import { styled } from "@mui/material";
 
 const LinkStyled = styled(Link)(() => ({
-  height: '70px',
-  width: '180px',
-  overflow: 'hidden',
-  display: 'block',
+  height: "70px",
+  width: "180px",
+  overflow: "hidden",
+  display: "block",
 }));
 
 const Logo = () => {
   return (
-    <LinkStyled to="/">
-      <LogoDark height={70} />
+    <LinkStyled
+      to="/"
+      height={70}
+      style={{
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <LogoDark1 />
     </LinkStyled>
-  )
+  );
 };
 
 export default Logo;
--- package/src/main.jsx
+++ package/src/main.jsx
@@ -1,13 +1,17 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Suspense>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Suspense>
-);
+  </Suspense>,
+)
